a920ae7347d2a5930c97d54dfb8c54512b7d8585
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2025-10-29 09:33:14 -0300

    :lipstick: feat: Adicionando componente Stepper

--- Anotafy/client/src/pages/Login/Register/index.tsx
+++ Anotafy/client/src/pages/Login/Register/index.tsx
@@ -3,6 +3,7 @@ import type { StatusEmpresa } from "../../../types/enums/StatusEmpresa";
 import type { StatusUnidade } from "../../../types/enums/StatusUnidade";
 import type { TipoUnidade } from "../../../types/enums/TipoUnidade";
 import Logo from "../../../components/Logo";
+import Stepper from "../../../components/Stepper";
 
 interface IRegisterEmpresa {
     razao_social: string;
@@ -50,24 +51,24 @@ interface IRegisterUsuario {
 
 export default function Register() {
     const [empresa, setEmpresa] = useState<IRegisterEmpresa>();
-    const [registerLevel, setRegisterLevel] = useState<number>(0);
+    const [registerLevel, setRegisterLevel] = useState<number>(1);
+
+    const handleNextLevel = () => {
+        setRegisterLevel((prev) => prev + 1);
+    };
 
     return (
-        <div className="flex justify-center items-center w-full my-h-screen bg-backgound">
-            <div className="relative flex bg-foreground py-6 w-4/5 h-4/5 rounded-lg p-4">
-                <div className="absolute -top-15 right-0 left-0 w-full justify-center flex items-center gap-2">
-                    <Logo className="w-10" />
-                    <h1 className="font-black text-xl select-none">ANOTAFY</h1>
-                </div>
-                <div className="h-20 w-full bg-red-300">
-                    <span></span>
-                    <div></div>
-                </div>
-                {registerLevel === 0 && (
-                    <div>
-
-                    </div>
-                )}
+        <div className="flex flex-col gap-3 justify-center items-center w-full my-h-screen bg-backgound">
+            <div className="top-15 right-0 left-0 w-full justify-center flex items-center gap-2">
+                <Logo className="w-10" />
+                <h1 className="font-black text-xl select-none">ANOTAFY</h1>
+            </div>
+            <div className="relative flex flex-col bg-foreground py-6 w-5/6 md:w-4/5 h-4/5 rounded-lg p-4 shadow-lg ">
+                <Stepper
+                    currentStep={registerLevel}
+                    totalSteps={5}
+                />
+                <div>{}</div>
             </div>
         </div>
     );
